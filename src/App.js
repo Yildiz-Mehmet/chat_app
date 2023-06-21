541e5f214f1f2beee720e88d52c0e77781013f11
yıldız <mesel4461@gmail.com>
2023-06-21 13:08:51 +0300

    as

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { ChatEngine } from "react-chat-engine";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <ChatEngine
+        height="100vh"
+        projectID="fda74612-e4cb-4add-8df1-99e0f1e1be6d"
+        userName="MehmetYILDIZ"
+        userSecret="Tektanem1."
+      />
     </div>
   );
 }
